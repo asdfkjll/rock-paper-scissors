462c6d596092383b16c2f000bd2dfcdea2544599
asdfkjll <124416006+asdfkjll@users.noreply.github.com>
2026-04-22 17:34:49 -0400

    output elements HTML created to show the results of the playRound function and css style added to them

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ console.log("hello world")
 let humanScore = 0
 let computerScore = 0
 const rounds = 5
-
-const options = {
-    "rock": 1,
-    "paper": 2,
-    "scissors": 3,
-}
+const options = { "rock": 1, "paper": 2, "scissors": 3  }
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1
@@ -47,23 +42,22 @@ container.addEventListener("click", (event) => {
     const targetId = event.target.id
     let humanChoice = null
     let computerChoice = getComputerChoice()
+    let winner = null
 
     switch(targetId) {
 	case "rock":
 	    humanChoice = 1 
-	    playRound(humanChoice, computerChoice)
 	    break
 	case "paper":
 	    humanChoice = 2 
-	    playRound(humanChoice, computerChoice)
 	    break
 	case "scissors":
 	    humanChoice = 3 
-	    playRound(humanChoice, computerChoice)
 	    break
     }
 
-    console.log(humanChoice, computerChoice)
+    winner = playRound(humanChoice, computerChoice)
+    console.log(humanChoice, computerChoice, winner)
 })
 
 
